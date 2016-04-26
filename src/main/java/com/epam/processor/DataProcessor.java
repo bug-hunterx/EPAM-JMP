071efaf52b1d22fd0e453b262372e0fd8306cd21
Bogdan Tkach <bagedan@gmail.com>
2016-04-26 22:37:08 +0800

    One failing test

--- src/main/java/com/epam/processor/DataProcessor.java
+++ src/main/java/com/epam/processor/DataProcessor.java
@@ -29,11 +29,11 @@ public class DataProcessor {
      * @return
      */
     public RoadAccident getAccidentByIndex7(String index){
-        for(RoadAccident accident : roadAccidentList) {
-            if(accident.getAccidentId().equals(index)) {
-                return accident;
-            }
-        }
+//        for(RoadAccident accident : roadAccidentList) {
+//            if(accident.getAccidentId().equals(index)) {
+//                return accident;
+//            }
+//        }
 
         return null;
     }
