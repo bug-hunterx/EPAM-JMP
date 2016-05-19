4cbc11af2df2214d070445ebc66d6aa34240add2
fpem <pengfj@gmail.com>
2016-05-19 18:38:17 +0800

    Task for implement the service method

---
+++ src/main/java/com/epam/mentoring/restapi/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.epam.mentoring.restapi.controller;
+
+/**
+ * Created by pengfrancis on 16/5/19.
+ */
+public class EmployeeController {
+    //todo add Restful services to getAll
+
+    //todo add Restful services to get
+
+
+    //todo add Restful services to getByName
+
+
+    //todo add Restful services to create,
+
+
+    //todo add Restful services to put, response 404(Not Found) when encounter the exception that the id is not exist
+
+
+    //todo add Restful services to delete
+
+
+}
