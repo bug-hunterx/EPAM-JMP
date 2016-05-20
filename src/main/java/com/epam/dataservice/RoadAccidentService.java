2268ee72feaaa3704d1f406484f28c1fc03a88a7
ocean <ocean_he@epam.com>
2016-05-20 14:12:33 +0800

    Homework3--update

--- src/main/java/com/epam/dataservice/RoadAccidentService.java
+++ src/main/java/com/epam/dataservice/RoadAccidentService.java
@@ -2,9 +2,11 @@ package com.epam.dataservice;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import com.epam.data.RoadAccident;
 
@@ -15,7 +17,7 @@ public class RoadAccidentService implements Runnable {
 	private static String DAY_TIME_EVENING = "EVENING";
 	private static String DAY_TIME_NIGHT = "NIGHT";
 
-	private static int NUMBER_OF_THREAD = 3;
+	private static int NUMBER_OF_THREAD = 6;
 	
 	private BlockingQueue<RoadAccident> accidentReaderQueue;
 	private BlockingQueue<RoadAccident> daytimeQueue;
@@ -40,7 +42,7 @@ public class RoadAccidentService implements Runnable {
 		this.policeForceService = policeForceService;
 	}
 
-	public ExecutorService readFromRoadAccientFileTo(BlockingQueue<RoadAccident> readerQueue) {
+	public static ExecutorService readFromRoadAccientFileTo(BlockingQueue<RoadAccident> readerQueue) {
 		ExecutorService readerExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREAD);
 		for (String accidentFile : roadAccidentFiles) {
 			readerExecutor.execute(new RoadAccidentReader(accidentFile, readerQueue));
@@ -48,12 +50,25 @@ public class RoadAccidentService implements Runnable {
 		return readerExecutor;
 	}
 	
-	public Thread writeRoadAccidentToFile(BlockingQueue<RoadAccident> accidentQueue, String fileName) {
+	public static Thread writeRoadAccidentToFile(BlockingQueue<RoadAccident> accidentQueue, String fileName) {
 		Thread writerThread = new Thread(new RoadAccidentWriter(fileName, accidentQueue));
 		writerThread.start();
 		return writerThread;
 	}  
 
+	public static List<Thread> startRoadAccidentService(BlockingQueue<RoadAccident> readerQueue,  
+			BlockingQueue<RoadAccident> dayTimeQueue, BlockingQueue<RoadAccident> nightTimeQueue) {
+		List<Thread> serviceThreadList = new ArrayList<Thread>();
+		PoliceForceService policeForceService = new PoliceForceService();
+		for (int i = 0; i < NUMBER_OF_THREAD; i++) {
+			Thread serviceThread = new Thread(new RoadAccidentService(readerQueue, dayTimeQueue, nightTimeQueue, policeForceService));
+			serviceThread.start();
+			serviceThreadList.add(serviceThread);
+		}
+		return serviceThreadList;
+	} 
+
+	
 	@Override
 	public void run() {
 		loadAccidentDataIntoQByDayTime();
@@ -99,5 +114,31 @@ public class RoadAccidentService implements Runnable {
 
 	public void setPoliceForceService(PoliceForceService policeForceService) {
 		this.policeForceService = policeForceService;
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+//		BlockingQueue<RoadAccident> accidentReaderQueue = new ArrayBlockingQueue<RoadAccident>(5000);
+//		BlockingQueue<RoadAccident> daytimeQueue = new ArrayBlockingQueue<RoadAccident>(5000);
+//		BlockingQueue<RoadAccident> nighttimeQueue = new ArrayBlockingQueue<RoadAccident>(5000);
+//
+//		ExecutorService readerExecutor = readFromRoadAccientFileTo(accidentReaderQueue);
+//		List<Thread> serviceThreadList = startRoadAccidentService(accidentReaderQueue, daytimeQueue, nighttimeQueue);
+//		Thread daytimeWriterThread = writeRoadAccidentToFile(daytimeQueue, "src/main/resources/DaytimeAccidents.csv");
+//		Thread nighttimeWriterThread = writeRoadAccidentToFile(nighttimeQueue,
+//				"src/main/resources/NighttimeAccidents.csv");
+//
+//		readerExecutor.shutdown();
+//		readerExecutor.awaitTermination(5, TimeUnit.MINUTES);
+//		while (!accidentReaderQueue.isEmpty())
+//			Thread.sleep(1000);
+//		for (Thread processorThread : serviceThreadList)
+//			processorThread.interrupt();
+//		while (!daytimeQueue.isEmpty())
+//			Thread.sleep(1000);
+//		daytimeWriterThread.interrupt();
+//		while (!nighttimeQueue.isEmpty())
+//			Thread.sleep(1000);
+//		nighttimeWriterThread.interrupt();
+
 	}
 }
