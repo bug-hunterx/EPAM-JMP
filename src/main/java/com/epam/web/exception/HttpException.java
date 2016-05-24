f849b75dbfb9a057e1ae9ab33cd03e88a29c01a1
RahulMuj <rahul_mujnani@epam.com>
2016-05-24 23:16:47 +0530

    Exception Class to handle HTTP Exceptions.

---
+++ src/main/java/com/epam/web/exception/HttpException.java
@@ -0,0 +1,28 @@
+package com.epam.web.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * Created by rahul.mujnani on 23.05.2016.
+ */
+public abstract class HttpException extends RuntimeException{
+    private HttpStatus httpStatus;
+    private String message;
+
+    public HttpException(HttpStatus httpStatus){
+        this.httpStatus= httpStatus;
+        //if(httpStatus!=null)
+
+    }
+    public HttpException(HttpStatus httpStatus, String message){
+        this.httpStatus= httpStatus;
+        this.message= message;
+
+    }
+    public HttpStatus getStatusCode(){
+        return httpStatus;
+    }
+    public String getMessage(){
+        return message;
+    }
+}
