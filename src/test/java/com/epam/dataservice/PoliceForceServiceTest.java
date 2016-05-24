125738f505169e004932c39b284bb18150e24e9e
Keane Yan <szsyyk@163.com>
2016-05-24 19:28:04 +0800

    refactoring

--- src/test/java/com/epam/dataservice/PoliceForceServiceTest.java
+++ src/test/java/com/epam/dataservice/PoliceForceServiceTest.java
@@ -2,20 +2,12 @@ package com.epam.dataservice;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(MockitoJUnitRunner.class)
 public class PoliceForceServiceTest {
 
-    private PoliceForceService service;
-
-    @Before
-    public void init(){
-        service = new PoliceForceService();
-    }
+    private PoliceForceService service = new PoliceForceService();
 
     @Test
     public void givenExistingPoliceForceNameThenShouldReturnCorrectContactNo(){
