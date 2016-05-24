50ab91434626b8a102344e907c7a8dc281ad97bf
northzzn <northzzn@hotmail.com>
2016-05-24 23:04:45 +0800

    HW#6

--- src/main/java/com/epam/mentoring/restapi/controller/EmployeeController.java
+++ src/main/java/com/epam/mentoring/restapi/controller/EmployeeController.java
@@ -1,24 +1,66 @@
 package com.epam.mentoring.restapi.controller;
 
+import java.util.List;
+
+import javax.xml.ws.http.HTTPException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.epam.mentoring.restapi.modal.Employee;
+import com.epam.mentoring.restapi.repository.EmployeeRepository;
+
 /**
  * Created by pengfrancis on 16/5/19.
  */
+@RestController
 public class EmployeeController {
-    //todo add Restful services to getAll
 
-    //todo add Restful services to get
+	@Autowired
+	private EmployeeRepository employeeRepository;
 
+	// todo add Restful services to getAll
+	@RequestMapping(method = RequestMethod.GET, value = "/employee/getall")
+	public @ResponseBody List<Employee> getAll() {
+		return employeeRepository.findAll();
+	}
 
-    //todo add Restful services to getByName
+	// todo add Restful services to get
+	@RequestMapping(method = RequestMethod.GET, value = "/employee/get/{id}")
+	public @ResponseBody Employee get(@PathVariable Long id) {
+		return employeeRepository.findOne(id);
+	}
 
+	// todo add Restful services to getByName
+	@RequestMapping(method = RequestMethod.GET, value = "/employee/getbyname/{name}")
+	public @ResponseBody List<Employee> getByName(@PathVariable String name) {
+		return employeeRepository.findByName(name);
+	}
 
-    //todo add Restful services to create,
+	// todo add Restful services to create,
+	@RequestMapping(method = RequestMethod.POST, value = "/employee/create")
+	public void create(@RequestBody Employee employee) {
+		employeeRepository.save(employee);
+	}
 
+	// todo add Restful services to put, response 404(Not Found) when encounter
+	// the exception that the id is not exist
+	@RequestMapping(method = RequestMethod.PUT, value = "/employee/put")
+	public void put(@RequestBody Employee employee) {
+		if (employeeRepository.findOne(employee.getId()) == null) {
+			throw new HTTPException(404);
+		}
+		employeeRepository.save(employee);
+	}
 
-    //todo add Restful services to put, response 404(Not Found) when encounter the exception that the id is not exist
-
-
-    //todo add Restful services to delete
-
-
+	// todo add Restful services to delete
+	@RequestMapping(method = RequestMethod.POST, value = "/employee/delete/{id}")
+	public void delete(@PathVariable Long id) {
+		employeeRepository.delete(id);
+	}
 }
--- src/main/java/com/epam/mentoring/restapi/repository/EmployeeRepository.java
+++ src/main/java/com/epam/mentoring/restapi/repository/EmployeeRepository.java
@@ -1,12 +1,21 @@
 package com.epam.mentoring.restapi.repository;
 
-import com.epam.mentoring.restapi.modal.Employee;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.epam.mentoring.restapi.modal.Employee;
 
 /**
  * Created by pengfrancis on 16/5/19.
  */
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+	@Query(value = "SELECT * FROM Employee WHERE name := name")
+	List<Employee> findByName(@Param("name") String name);
+
 }
