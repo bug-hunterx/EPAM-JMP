275060077da2574ed7cc25cb7d132715c9f1a432
RahulMuj <rahul_mujnani@epam.com>
2016-05-25 22:26:04 +0530

    Integration Test For File Writer Functionality
    Assignment Task-3

---
+++ src/test/java/com/epam/integration/tests/AccidentIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.epam.integration.tests;
+
+import com.epam.Main;
+import com.epam.data.AccidentsDataLoader;
+import com.epam.data.RoadAccident;
+import com.epam.processor.DataProcessor;
+import com.epam.report.DataReportGenerator;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by rahul.mujnani on 5/05/2016.
+ */
+@Category(IntegrationTest.class)
+public class AccidentIntegrationTest {
+        private static final String DAYTIME_ACCIDENTS_CSV = "src/main/resources/DaytimeAccidents.csv";
+        private static final String NIGHTTIME_ACCIDENTS_CSV = "src/main/resources/NighttimeAccidents.csv";
+
+
+        @Test
+        public void should_Generate_AccidentRecords() {
+            //Main.main();
+            File dayfile = new File(DAYTIME_ACCIDENTS_CSV);
+            Assert.assertTrue(dayfile.exists() && !dayfile.isDirectory());
+            File nightfile = new File(NIGHTTIME_ACCIDENTS_CSV);
+            Assert.assertTrue(nightfile.exists() && !nightfile.isDirectory());
+        }
+}
+
+
+
+
